b256592df521dd43ef0131512730f64124f2c6bf
Jordan <148658758+jalex-op@users.noreply.github.com>
2023-11-03 14:50:34 -0230

    added slider to inc/dec grid resolution

--- js-script.js
+++ js-script.js
@@ -4,9 +4,17 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    const container = document.getElementById('grid-container');
-    const totalSquares = 16 * 16;
-  
+  const container = document.getElementById('grid-container');
+  let resolution = 16; // Start with 16x16 resolution
+
+  // Function to create squares
+  function createGrid(resolution) {
+    container.innerHTML = ''; // Clear the existing grid
+    container.style.gridTemplateColumns = `repeat(${resolution}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${resolution}, 1fr)`;
+
+    const totalSquares = resolution * resolution;
+
     for (let i = 0; i < totalSquares; i++) {
       const square = document.createElement('div');
       square.classList.add('square');
@@ -15,16 +23,27 @@ document.addEventListener('DOMContentLoaded', function() {
       });
       container.appendChild(square);
     }
-  
-    // Function to reset the color of all squares
-    function resetSquares() {
-      const squares = document.querySelectorAll('.square');
-      squares.forEach(square => {
-        square.style.backgroundColor = '#f0f0f0'; // Original color
-      });
-    }
-  
-    // Event listener for the reset button
-    const resetButton = document.getElementById('reset-button');
-    resetButton.addEventListener('click', resetSquares);
+  }
+
+  // Initially create a 16x16 grid
+  createGrid(resolution);
+
+  // Function to reset the color of all squares
+  function resetSquares() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+      square.style.backgroundColor = 'rgb(194, 191, 180)'; // Original color
+    });
+  }
+
+  // Event listener for the reset button
+  const resetButton = document.getElementById('reset-button');
+  resetButton.addEventListener('click', resetSquares);
+
+  // Event listener for the resolution slider
+  const slider = document.getElementById('resolution-slider');
+  slider.addEventListener('input', function() {
+    resolution = slider.value;
+    createGrid(resolution); // Re-create the grid with the new resolution
   });
+});
